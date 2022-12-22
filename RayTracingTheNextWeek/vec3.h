127885f63ef5ec7323c74dc4fa663177ebaa4c63
VictorHSMoura <victorhugosmoura@gmail.com>
2022-12-22 19:44:37 -0300

    Fix near_zero bug and apply formatting to rtweekend.

--- RayTracingTheNextWeek/vec3.h
+++ RayTracingTheNextWeek/vec3.h
@@ -53,7 +53,7 @@ class vec3 {
     bool near_zero() const {
         // Return true if the vector is close to zero in all dimensions.
         const auto s = 1e-8;
-        return (fabs(e[0] < s)) && (fabs(e[1] < s)) && (fabs(e[2] < s));
+        return (fabs(e[0]) < s) && (fabs(e[1]) < s) && (fabs(e[2]) < s);
     }
 
    public:
